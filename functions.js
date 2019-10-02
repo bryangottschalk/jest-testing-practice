9a30599e1974dad2619bf081a541e180796aa4b6
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-10-02 17:11:17 -0500

    initial commit

---
+++ functions.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+const functions = {
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: x => x,
+  createUser: () => {
+    const user = { firstName: 'Bryan', lastName: 'Gottschalk' };
+    return user;
+  },
+  fetchUser: async () => {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/users/1'
+    );
+    return data;
+  },
+  // promise chain version
+  fetchUserWithPromiseChaining: () =>
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .then(res => res.data)
+      .catch(err => 'error'),
+};
+
+module.exports = functions;
